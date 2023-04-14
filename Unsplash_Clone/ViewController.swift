628c9781b05f0fa1ef03ac90dcc7fc6490df8022
wongbingg <wb277@naver.com>
2023-04-14 14:36:46 +0900

    Initial Commit

---
+++ Unsplash_Clone/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Unsplash_Clone
+//
+//  Created by 이원빈 on 2023/04/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray
+    }
+}
