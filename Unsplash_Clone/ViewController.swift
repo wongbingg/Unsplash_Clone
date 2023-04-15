9146e026edecdaa1e68e5336400bc0e712faa8d1
wongbingg <wb277@naver.com>
2023-04-15 18:06:00 +0900

    feat : "ViewController" preview 테스트 #1
    - preview가 잘 동작하는지 예시코드 작성

--- Unsplash_Clone/ViewController.swift
+++ Unsplash_Clone/ViewController.swift
@@ -12,5 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray
+        let text = UILabel()
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.text = "Preview Tests"
+        text.textColor = .black
+        view.addSubview(text)
+        text.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        text.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        ViewController().showPreview()
+    }
+}
+#endif
